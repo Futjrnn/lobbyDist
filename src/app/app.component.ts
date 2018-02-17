c68ef5918b959109ce0215d464545346ec7f85ab
Levi Morris <levi.morris@campspot.com>
2018-02-17 00:28:25 -0700

    Web3: JUST AS EASY AS ALL THAT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import * as _ from 'lodash';
+import Web3 from 'web3';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,14 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'lobbyDist';
+  web3: any;
+
+  @HostListener('window:load')
+  windowLoaded() {
+    this.initWeb3();
+  }
+
+  initWeb3 = () => {
+    window.web3 = _.isUndefined(window.web3) ? new Web3(new Web3.providers.HttpProvider('http://localhost:8545')) : new Web3(window.web3.currentProvider);
+  }
 }
---
+++ src/app/web3-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+declare global {
+  interface Window { web3: any }
+}
+
+window.web3 = window.web3 || {};
+
+@Injectable()
+export class Web3Service {
+  web3: any;
+
+  constructor() {
+    this.web3 = window.web3;
+  }
+
+  getWeb3() {
+    return this.web3;
+  }
+}
+
