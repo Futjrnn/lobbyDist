a7e2e59933c176e9346bb26e767bb979171f2b3b
Alex Reynolds <alex.reynolds@campspot.com>
2018-02-17 11:43:26 -0700

    made the routing work between the list view and the create new issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NewIssueComponent } from './new-issue/new-issue.component';
 import { IssueDetailViewComponent } from './issue-detail-view/issue-detail-view.component'
+import { IssuesListComponent } from './issues-list/issues-list.component';
 
 const routes: Routes = [
+  {path: '', component: IssuesListComponent},
   {path: 'issue/new', component: NewIssueComponent},
   {path: 'issue/view', component: IssueDetailViewComponent}
 ]
