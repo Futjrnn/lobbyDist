c887a8b7f017c2f183e8117f0cfaaaae35abf253
CHANSE HORTON <chansehorton@yahoo.com>
2018-02-16 23:36:31 -0700

    add basic components for front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IssueServiceService } from './issue-service.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IssuesListComponent } from './issues-list/issues-list.component';
+import { IssueCardComponent } from './issue-card/issue-card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IssuesListComponent,
+    IssueCardComponent
   ],
   imports: [
     BrowserModule
