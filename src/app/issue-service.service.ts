99f588b5e0b08830475c1e004e1b3d0a9bc41d1b
Alex Reynolds <alex.reynolds@campspot.com>
2018-02-16 23:18:47 -0700

    added basic issueService and method for getting mock issues

--- src/app/issue-service.service.ts
+++ src/app/issue-service.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Issue } from "./issue";
+import { ISSUES } from './mock-issues';
+
 
 @Injectable()
 export class IssueServiceService {
 
+  issues = ISSUES
+
   constructor() { }
+
+  // getIssues
+    getIssues(): Issue[] {
+      return ISSUES;
+    }
+  // createIssue
+
+  // supportIssue
+
+  // collectIssue
+
+  // closeIssue
 
 }
