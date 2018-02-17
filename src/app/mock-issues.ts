56dadb9f34475f449aae10db1cd881d027db5140
Alex Reynolds <alex.reynolds@campspot.com>
2018-02-16 23:17:13 -0700

    added mock-issues

---
+++ src/app/mock-issues.ts
@@ -0,0 +1,7 @@
+import { Issue } from "./issue";
+
+export const ISSUES: Issue[] = [
+    { id: 1, title: "net neutrality", description: "muh internets" },
+    { id: 2, title: "3d printed guns", description: "muh guns" },
+    { id: 3, title: "pro choice", description: "muh babbies" }
+]
