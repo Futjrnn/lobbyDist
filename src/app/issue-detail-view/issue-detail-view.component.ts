a1480ca25979315ff62baf45ed7b539a347ebed8
logeyg <logan.gabriel@campspot.com>
2018-02-17 23:29:07 -0700

    sending transactions via metamask

--- src/app/issue-detail-view/issue-detail-view.component.ts
+++ src/app/issue-detail-view/issue-detail-view.component.ts
@@ -11,11 +11,28 @@ import {Observable} from 'rxjs/Observable';
 export class IssueDetailViewComponent implements OnInit {
   // @Input() issue: Issue;
   private issue: Issue;
+  private web3: any;
 
   constructor(public issueService: IssueServiceService) { }
 
   ngOnInit() {
     this.issue = this.issueService.currentIssue;
+    this.web3 = window.web3;
+    console.log(this.web3);
   }
 
+  supportIssue(issue: Issue) {
+
+  	//let toAddress = issue.address;
+  	var amount = web3.toWei(0.01, "ether")
+
+  	let transactionObject = {};
+  	transactionObject.to = "0x156d7a972B64C8d3e0454277244BfeaAAE15D201";
+  	transactionObject.value = amount;
+  	transactionObject.gas = 21000;
+
+  	this.web3.eth.sendTransaction(transactionObject, function(err, transactionHash) {
+  		if (!err) console.log(transactionHash); // "0x7f9fade1c0d57a7af66ab4ead7c2eb7b11a91385"
+	});
+  }
 }
